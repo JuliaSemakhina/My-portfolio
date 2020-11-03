2d0898acffd849be6e689277108db5cfdb4cb493
Julia Semakhina <contremoi@yandex.ru>
2020-11-03 23:50:20 +0300

    changing design

--- script.js
+++ script.js
@@ -1,4 +1,36 @@
+const menuBtn = document.querySelector('.toggler');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBrand = document.querySelector('.menu-brand');
+const navItems = document.querySelectorAll('.nav-item');
+const sideBar = document.querySelector('.sidebar');
 
+let showMenu = false;
 
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+	if(!showMenu) {
+		sideBar.classList.add('close');
+		menu.classList.add('show');
+		menuNav.classList.add('show');
+		menuBrand.classList.add('show');
+		navItems.forEach(item=> {
+			item.classList.add('show')
+		});
+
+		showMenu = true;
+	} else {
+		sideBar.classList.remove('close');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBrand.classList.remove('show');
+		navItems.forEach(item=> {
+			item.classList.remove('show')
+		});
+
+		showMenu = false;
+	}
+}
 
           
