dfab45e4a23b7db5c2d1f2bd20e9d1d01bc56447
Julia Semakhina <contremoi@yandex.ru>
2020-11-11 18:20:53 +0300

    updating files

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const menuBtn = document.querySelector('.toggler');
+const menuBtn = document.querySelector('.nav-button');
 const menu = document.querySelector('.menu');
 const menuNav = document.querySelector('.menu-nav');
 const menuBrand = document.querySelector('.menu-brand');
 const navItems = document.querySelectorAll('.nav-item');
+const navLinks = document.querySelectorAll('.nav-link');
 const sideBar = document.querySelector('.sidebar');
 
 let showMenu = false;
@@ -12,6 +13,7 @@ menuBtn.addEventListener('click', toggleMenu);
 function toggleMenu() {
 	if(!showMenu) {
 		sideBar.classList.add('close');
+		menuBtn.classList.add('show');
 		menu.classList.add('show');
 		menuNav.classList.add('show');
 		menuBrand.classList.add('show');
@@ -22,6 +24,7 @@ function toggleMenu() {
 		showMenu = true;
 	} else {
 		sideBar.classList.remove('close');
+		menuBtn.classList.remove('show');
 		menu.classList.remove('show');
 		menuNav.classList.remove('show');
 		menuBrand.classList.remove('show');
@@ -33,4 +36,16 @@ function toggleMenu() {
 	}
 }
 
-          
+navLinks.forEach(link => {
+	link.addEventListener('click', () => {
+		sideBar.classList.remove('close');
+		menuBtn.classList.remove('show');
+		menu.classList.remove('show');
+		menuNav.classList.remove('show');
+		menuBrand.classList.remove('show');
+		navItems.forEach(item=> {
+			item.classList.remove('show')
+		});
+		showMenu = false;
+	})
+})
